38d2561956e6d4bf12b624d5198beeb493bd7981
mauvehed <mauvehed@users.noreply.github.com>
2019-10-21 08:13:45 -0500

    feature: Fix script not printing Chores

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Version 2 (2019-10-21)
 
 ## Features
-* feature: Added index.js script ([bc8ad4])(https://github.com/mauvehed/changelog/commit/bc8ad41934eff93d8d139bb94159cdde839d7faf))
+* Added index.js script ([bc8ad4](https://github.com/mauvehed/changelog/commit/bc8ad41934eff93d8d139bb94159cdde839d7faf))
 
+## Chores
+* Bump to version 2 ([a73777](https://github.com/mauvehed/changelog/commit/a73777775eefb4a489653ec1e5f403d837027a54))
+* Bump version to 1.1 ([581610](https://github.com/mauvehed/changelog/commit/581610b29b9fd82f9c5e31d240ee88c8c29d5cc4))
+* Bump to version 1.1, add based code to index.js ([e47a54](https://github.com/mauvehed/changelog/commit/e47a54d7f8d41a6523012f0223c6c09c5639273b))
+* Initial commit for changelog generator ([449121](https://github.com/mauvehed/changelog/commit/4491210286c004aaa450b0d0fb819fdfcd30548e))
--- index.js
+++ index.js
@@ -1,63 +1,67 @@
-// https://medium.com/better-programming/create-your-own-changelog-generator-with-git-aefda291ea93
-
-const child = require('child_process');
+const child = require("child_process");
 const fs = require("fs");
 
 const output = child
-    .execSync(`git log --format=%B%H-----DELIMITER-----`)
-    .toString('utf-8');
+  .execSync(`git log --format=%B%H----DELIMITER----`)
+  .toString("utf-8");
 
 const commitsArray = output
-    .split('-----DELIMITER-----\n')
-    .map(commit => {
-        const [message, sha] = commit.split('\n');
+  .split("----DELIMITER----\n")
+  .map(commit => {
+    const [message, sha] = commit.split("\n");
 
-        return { sha, message};
-})
-.filter(commit => Boolean(commit.sha));
+    return { sha, message };
+  })
+  .filter(commit => Boolean(commit.sha));
 
 const currentChangelog = fs.readFileSync("./CHANGELOG.md", "utf-8");
 const currentVersion = Number(require("./package.json").version);
 const newVersion = currentVersion + 1;
-
 let newChangelog = `# Version ${newVersion} (${
-    new Date().toISOString().split("T")[0]
+  new Date().toISOString().split("T")[0]
 })\n\n`;
 
 const features = [];
 const chores = [];
 
 commitsArray.forEach(commit => {
-    if (commit.message.startsWith("feature: ")) {
-        features.push(
-            `* ${commit.message.replace("feature :", "")} ([${commit.sha.substring(0,6)}])(https://github.com/mauvehed/changelog/commit/${commit.sha}))\n`
-        );
-    }
-    if (commit.message.startsWith("chores: ")) {
-        chores.push(
-            `* ${commit.message.replace("chores :", "")} ([${commit.sha.substring(0,6)}])(https://github.com/mauvehed/changelog/commit/${commit.sha}))\n`
-        );
-    }
+  if (commit.message.startsWith("feature: ")) {
+    features.push(
+      `* ${commit.message.replace("feature: ", "")} ([${commit.sha.substring(
+        0,
+        6
+      )}](https://github.com/mauvehed/changelog/commit/${
+        commit.sha
+      }))\n`
+    );
+  }
+  if (commit.message.startsWith("chore: ")) {
+    chores.push(
+      `* ${commit.message.replace("chore: ", "")} ([${commit.sha.substring(
+        0,
+        6
+      )}](https://github.com/mauvehed/changelog/commit/${
+        commit.sha
+      }))\n`
+    );
+  }
 });
 
 if (features.length) {
-    newChangelog += `## Features\n`;
-    features.forEach(feature => {
-        newChangelog += feature;
-    });
-    newChangelog += '\n';
+  newChangelog += `## Features\n`;
+  features.forEach(feature => {
+    newChangelog += feature;
+  });
+  newChangelog += '\n';
 }
 
 if (chores.length) {
-    newChangelog += `## Chores\n`;
-    chores.forEach(chore => {
-        newChangelog += chore;
-    });
-    newChangelog += '\n';
+  newChangelog += `## Chores\n`;
+  chores.forEach(chore => {
+    newChangelog += chore;
+  });
+  newChangelog += '\n';
 }
 
 // prepend the newChangelog to the current one
 fs.writeFileSync("./CHANGELOG.md", `${newChangelog}${currentChangelog}`);
-
-// Debug log
-// console.log({ commitsArray });
